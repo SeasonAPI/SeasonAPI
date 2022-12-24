06573839528914431e4b7b76d30994c6d7a7922f
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-12-24 20:48:46 +0600

    Comment keyGen

--- ts-funcs/api/keyGen.ts
+++ ts-funcs/api/keyGen.ts
@@ -1,6 +1,9 @@
+/**
+ * The `generateApiKey()` function is used to generate random API Keys with 32 characters
+ */
+
 import * as crypto from "crypto";
 
-// Function to generate a new API key
 function generateApiKey() {
   return crypto.randomBytes(32).toString("hex");
 }
