3d299804647bce75aac736d94504221532e5334e
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-01-12 23:53:47 +0600

    fix: fix API Key generator

--- src/functions/api.ts
+++ src/functions/api.ts
@@ -4,15 +4,9 @@ import { randomBytes } from "crypto";
  * Function to generate the API Key.
  * @returns API Key
  */
-const generateApiKey = () => {
-  return new Promise((resolve, reject) => {
-    randomBytes(32, (err, buffer) => {
-      if (err) {
-        return reject(err);
-      }
-      resolve(buffer.toString("hex"));
-    });
-  });
-};
+import * as crypto from "crypto";
 
+function generateApiKey() {
+  return crypto.randomBytes(32).toString("hex");
+}
 export { generateApiKey };
