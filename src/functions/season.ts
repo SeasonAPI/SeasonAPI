9c51512dfc2b06c79f7f3b0e639023183190ea61
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-01-14 15:11:16 +0600

    chore: move the `getCurrentSeason()` function to handler

--- src/functions/season.ts
+++ src/functions/season.ts
@@ -19,4 +19,33 @@ const getCurrentSeasonBDINNL = () => {
   return currentSeason;
 };
 
-export { getCurrentSeasonBDINNL };
+const getSouthPoleSeason = () => {
+  let currentDate = new Date();
+  let currentMonth = currentDate.getMonth();
+  let season;
+  if (currentMonth >= 11 || currentMonth <= 2) {
+    season = "winter";
+  } else {
+    season = "summer";
+  }
+  let currentSeason = season.charAt(0).toUpperCase() + season.slice(1);
+  return currentSeason;
+};
+const getCurrentSeason = () => {
+  const date = new Date();
+  const currentMonth = date.getMonth();
+  let season;
+
+  if (currentMonth <= 3 && currentMonth >= 5) {
+    season = "spring";
+  } else if (currentMonth <= 6 && currentMonth >= 8) {
+    season = "summer";
+  } else if (currentMonth <= 9 && currentMonth >= 11) {
+    season = "autumn";
+  } else {
+    season = "winter";
+  }
+  let currentSeason = season.charAt(0).toUpperCase() + season.slice(1);
+  return currentSeason;
+};
+export { getCurrentSeasonBDINNL, getSouthPoleSeason, getCurrentSeason };
