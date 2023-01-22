400347a25fd9dfc744fb1bb938dfcf0dcbfbc5bc
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-01-22 17:29:56 +0600

    chore: change `GetAPIKeyPage.html` to react

---
+++ client/src/pages/GetAPIKeyPage.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+import "./GetAPIKeyPage.css";
+
+const GetAPIKeyPage: FC = (props) => {
+  return (
+    <div className="GetAPIKeyPage">
+      <h1 className="text1">SeasonAPI Key Generator</h1>
+      <p className="text2">
+        Fill the form below and click the button to get yourself a nice valid
+        API Key.
+      </p>
+
+      <form method="post" action="/api/keys">
+        <div className="form-item">
+          <label htmlFor="name" className="Label1">
+            Name:
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="Input1"
+            placeholder="i.e: Joe"
+            required
+          />
+        </div>
+        <button type="submit" className="Button1">
+          Get Key
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default GetAPIKeyPage;
